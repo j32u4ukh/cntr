6c1829fd90f88bc93e2473b6cc08c0c73a75f31d
j32u4ukh <j32u4ukh@gmail.com>
2024-04-16 09:50:22 +0800

    feat: Set 元素可輸出成陣列

--- example/main.go
+++ example/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	a1 := cntr.NewArray(1, 2, 3, 4, 5)
+	a1 := cntr.NewSet(1, 2, 3, 4, 5)
 	a2 := a1.Clone()
-	a2.Append(9)
+	a2.Add(9)
 	fmt.Printf("a1: %+v\n", a1)
 	fmt.Printf("a2: %+v\n", a2)
 	iter := a2.GetIterator()
 	for iter.HasNext(){
 		e := iter.Next()
-		fmt.Printf("e: %v\n", e)
+		fmt.Printf("e: %+v\n", e)
 	}
 	fmt.Println("End")
 }
